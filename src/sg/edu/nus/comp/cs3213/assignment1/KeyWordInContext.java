3910dc994a36d5c186a67bf054670f3532d56ede
boyang <wangboyangjinghua@gmail.com>
2014-08-23 13:26:23 +0800

    on read file

--- src/sg/edu/nus/comp/cs3213/assignment1/KeyWordInContext.java
+++ src/sg/edu/nus/comp/cs3213/assignment1/KeyWordInContext.java
@@ -1,24 +1,28 @@
 package sg.edu.nus.comp.cs3213.assignment1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class KeyWordInContext {
 
+	private static final String FILE_NOT_FOUND_MSG = "The file you specified is not found";
 	private static final int READ_FILE_TYPE_IGNORE = 1;
 	private static final int READ_FILE_TYPE_ENTRY = 0;
-	
+
 	private static final int COMMAND_NUMBER_ADD_ENTRIES_MAN = 2;
 	private static final int COMMAND_NUMBER_ADD_ENTRIES_FILE = 1;
 	private static final int COMMAND_NUMBER_SEARCH = 5;
 	private static final int COMMAND_NUMBER_ADD_IGNORE_FILE = 3;
 	private static final int COMMAND_NUMBER_ADD_IGNORE_MAN = 4;
 	private static final int COMMAND_NUMBER_EXIT = 6;
-	
+
 	private static final String INVALID_COMMAND_MSG = "Invalid command. Your input is: %s\nCheck your input.";
 	private static final String EXIT_MSG = "Bye";
-	
+
 	private static final String MENU_INSTRUCTIONS = new StringBuilder(
 			"Welcome to KWIC! Choose your command followed by arguments. Please see command list and examples below.\n")
 			.append("\n")
@@ -31,21 +35,18 @@ public class KeyWordInContext {
 			.append("3. Add ignore words via text file e.g. 4 c:\\path\\to\\your\\file \n")
 			.append("4. Add ignore words by manual input, separated by space e.g. 5 orange strawberry \n")
 			.append("5. Search by keyword, for multiple keywords separate by space e.g. 3 apple banana \n")
-			.append("6. Exit e.g. 6\n")
-			.append("\n")
-			.toString();
-	
+			.append("6. Exit e.g. 6\n").append("\n").toString();
+
 	private static Boolean isExit = false;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
 
-		while (!isExit ) {
+		while (!isExit) {
 			System.out.println(MENU_INSTRUCTIONS);
 			String input = reader.readLine();
-			String output = 
-					processInput(input);
+			String output = processInput(input);
 			System.out.println(output);
 		}
 
@@ -53,37 +54,53 @@ public class KeyWordInContext {
 
 	private static String processInput(String input) {
 		input = input.trim();
-		
+
 		String output = String.format(INVALID_COMMAND_MSG, input);
 		int command = Integer.parseInt(input.substring(0, 1));
-		switch(command) {
-		case COMMAND_NUMBER_ADD_ENTRIES_FILE: 
+		switch (command) {
+		case COMMAND_NUMBER_ADD_ENTRIES_FILE:
 			String entryPath = input.substring(2);
-			ArrayList<String> entryList = readFileToList(entryPath, READ_FILE_TYPE_ENTRY);
+			ArrayList<String> entryList;
+			try {
+				entryList = readFileToList(entryPath,
+						READ_FILE_TYPE_ENTRY);
+				
+			} catch (FileNotFoundException e) {
+				output = FILE_NOT_FOUND_MSG;
+			}
 			break;
-		case COMMAND_NUMBER_ADD_ENTRIES_MAN: 
+		case COMMAND_NUMBER_ADD_ENTRIES_MAN:
 			break;
-		case COMMAND_NUMBER_SEARCH: 
+		case COMMAND_NUMBER_SEARCH:
 			break;
 		case COMMAND_NUMBER_ADD_IGNORE_FILE:
 			String ignorePath = input.substring(2);
-			ArrayList<String> ignoreList = readFileToList(ignorePath, READ_FILE_TYPE_IGNORE);
+			ArrayList<String> ignoreList;
+			try {
+				ignoreList = readFileToList(ignorePath,
+						READ_FILE_TYPE_IGNORE);
+				
+			} catch (FileNotFoundException e) {
+				output = FILE_NOT_FOUND_MSG;
+			}
 			break;
-		case COMMAND_NUMBER_ADD_IGNORE_MAN: 
+		case COMMAND_NUMBER_ADD_IGNORE_MAN:
 			break;
 		case COMMAND_NUMBER_EXIT:
 			isExit = true;
 			output = EXIT_MSG;
 			break;
-		default: 
+		default:
 		}
-		
+
 		return output;
 	}
 
 	private static ArrayList<String> readFileToList(String path,
-			int readFileType) {
-		
+			int readFileType) throws FileNotFoundException {
+		File f = new File(path);
+		BufferedReader r = new BufferedReader(new FileReader(f));
+
 		return null;
 	}
 
